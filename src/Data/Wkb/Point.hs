3a5bfd9c0c4110829043be3cc4218bf6e696746a
Dominic Endrei <dominic.endrei@gmail.com>
2018-08-29 09:14:45 +1000

    Checkpoint

--- src/Data/Wkb/Point.hs
+++ src/Data/Wkb/Point.hs
@@ -1,14 +1,29 @@
 module Data.Wkb.Point where
 
+import           Control.Monad     (foldM, forM, liftM, (>>=))
 import qualified Data.Binary.Get   as Get
 import qualified Data.Geospatial   as Geospatial
+import qualified Data.Int          as Int
 
 import qualified Data.Wkb.Endian   as Endian
 import qualified Data.Wkb.Geometry as Geometry
 
 getPoint :: Endian.EndianType -> Geometry.WkbCoordinateType -> Get.Get Geospatial.GeospatialGeometry
-getPoint endianType _ = do
+getPoint endianType coordType = do
+  geoPoint <- getGeoPoint endianType coordType
+  pure $ Geospatial.Point geoPoint
+
+getMultiPoint :: Endian.EndianType -> Geometry.WkbCoordinateType -> Get.Get Geospatial.GeospatialGeometry
+getMultiPoint endianType coordType =
+  Endian.getFourBytes endianType >>= (getPoints endianType coordType)
+
+getPoints :: Endian.EndianType -> Geometry.WkbCoordinateType -> Int.Int32 -> Get.Get Geospatial.GeospatialGeometry
+getPoints endianType coordType numberOfPoints = do
+  geoPoints <- forM [1..numberOfPoints] (\_ -> getGeoPoint endianType coordType)
+  pure $ Geospatial.MultiPoint $ Geospatial.mergeGeoPoints geoPoints
+
+getGeoPoint :: Endian.EndianType -> Geometry.WkbCoordinateType -> Get.Get Geospatial.GeoPoint
+getGeoPoint endianType _ = do
   x <- Endian.getDouble endianType
   y <- Endian.getDouble endianType
-  return $ Geospatial.Point $ Geospatial.GeoPoint [x,y]
-
+  pure $ Geospatial.GeoPoint [x,y]
