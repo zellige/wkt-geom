899fb0974f49fdb3b865a3628d490335b214a189
Dominic Endrei <dominic.endrei@gmail.com>
2019-01-04 15:20:33 +1000

    Fix test

--- src/Data/Internal/Wkb/GeometryCollection.hs
+++ src/Data/Internal/Wkb/GeometryCollection.hs
@@ -9,6 +9,7 @@ import qualified Data.Binary.Get            as BinaryGet
 import qualified Data.ByteString.Builder    as ByteStringBuilder
 import qualified Data.Foldable              as Foldable
 import qualified Data.Geospatial            as Geospatial
+import           Data.Monoid                ((<>))
 import qualified Data.Sequence              as Sequence
 
 import qualified Data.Internal.Wkb.Endian   as Endian
